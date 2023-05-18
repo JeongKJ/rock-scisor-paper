6af5cba111e5d122e7b92e13e4ab358c27835acd
JeongKJ <rudwls512@gmail.com>
2023-05-18 12:50:31 +0900

    second

--- src/App.js
+++ src/App.js
@@ -7,26 +7,107 @@ const random = (value) => { return Math.ceil(Math.random() * value) }
 function App() {
   const [first, setFirst] = useState([]);
   const [second, setSecond] = useState([]);
-
+  const [round, setRound] = useState(1);
+  const [one, setOne] = useState([]);
+  const [two, setTwo] = useState([]);
+  const [result, setResult] = useState('');
+  var none = document.querySelector('#button>button:first-of-type');
+  var block = document.querySelector('#button>button:nth-of-type(2)');
+  
   const playGame = () => {
     const redteam = random(3);
     const blueteam = random(3);
-    setFirst([...first, redteam]);
-    setSecond([...second, blueteam]);
-    console.log(first);
+    const win = '승리';
+    const lose = '패배';
+    const draw = '무승부'
+
+   
+    if (round <= 5) { 
+      setRound(round + 1);
+      setFirst([...first, redteam]);
+      setSecond([...second, blueteam]);
+    }
+    else if (round > 5) {
+      none.style.display = 'none';
+      block.style.display = 'block';
+      return false;
+    }
+    if (redteam === 1) {
+        if (blueteam === 1) {
+          setOne([...one, draw]);
+          setTwo([...two, draw]);
+      }
+      else if(blueteam === 2){
+          setOne([...one, win])
+          setTwo([...two, lose]);
+      }
+      else if(blueteam === 3){
+          setOne([...one, lose]);
+          setTwo([...two, win]);
+      }
+    }
+        if (redteam === 2) {
+        if (blueteam === 1) {
+          setOne([...one, lose]);
+          setTwo([...two, win]);
+      }
+      else if(blueteam === 2){
+          setOne([...one, draw]);
+          setTwo([...two, draw]);
+      }
+      else if(blueteam === 3){
+          setOne([...one, win])
+          setTwo([...two, lose]);
+      }
+    }
+        if (redteam === 3) {
+        if (blueteam == 1) {
+          setOne([...one, win])
+          setTwo([...two, lose]);
+        
+      }
+      else if(blueteam == 2){
+          setOne([...one, lose]);
+          setTwo([...two, win]);
+      }
+      else if(blueteam == 3){
+          setOne([...one, draw]);
+          setTwo([...two, draw]);
+      }
+    }
   }
-  const resetGame = () => {}
+  const resetGame = () => {
+    setFirst([]);
+    setSecond([]);
+    setRound(1);
+    setOne([]);
+    setTwo([]);
+    setResult('');
+    none.style.display = 'block';
+    block.style.display = 'none';
+  }
+  const gameResult = () => { 
+    if (first > second) {
+      setResult('A Team win.');
+    }
+    else {
+      setResult('B Team win.');
+    }
+  }
 
   return (
     <div id="box">
       <div id='contain'>
         <h1>Game</h1>
         <div id='button'>
-        <Button onClick={playGame}>Start</Button>
-        <Button onClick={resetGame}>Reset</Button>
+          <Button onClick={playGame}>Start</Button>
+          <Button onClick={gameResult}>Result</Button>
+          <Button onClick={resetGame}>Reset</Button>
         </div>
-        <Border name="팀 A" record={first}/>
-        <Border name="팀 B" record={second} />
+        <h2>{result}</h2>
+        <Border name="A Team" record={first} result={one} round={round} />
+        <h1>VS</h1>
+        <Border name="B Team" record={second} result={two} round={round}/>
       </div>
     </div>
   );
--- src/Border.js
+++ src/Border.js
@@ -1,14 +1,15 @@
 import Screen from "./Screen";
 
-export default function Border({ name, record }) {
-    console.log(record);
+export default function Border({ name, record, result, round }) {
+    const sum = record;
+    const num = record[record.length - 1];
     return (
         <div className="minicontain">
             <h2>{name}</h2>
-            <Screen />
-            <h2>승</h2>
+            <Screen num={num} />
+            <h2>{result[round-2]}</h2>
             <h2>결과</h2>
-            <p></p>
+            <p>{result.join(' , ')}</p>
         </div>
     );
 }
